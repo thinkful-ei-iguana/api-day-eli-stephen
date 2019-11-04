83ff4d448c6c552f1020fe84904881a65234f661
jpegzilla <eris@jpegzilla.com>
2019-11-04 11:50:27 -0500

    updated api/getItems

--- src/api.js
+++ src/api.js
@@ -1,16 +1,21 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/eli-stephen';
+const BASE_URL = "https://thinkful-list-api.herokuapp.com/eli-stephen";
 
-const getItems = function () {
-  return Promise.resolve('A successful response!');
+const getItems = function() {
+  return fetch(`${BASE_URL}/items`);
 };
 
-const createItem = function (name) {
+const createItem = function(name) {
   const newItem = JSON.stringify({
     name: name
   });
-  console.log(newItem);
-  return fetch(`${BASE_URL}/items`, {method: 'POST',   headers: new Headers({'Content-Type':        'application/json'}),
-    body: newItem});
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  console.log("headers", headers);
+  return fetch(`${BASE_URL}/items`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: newItem
+  });
 };
 
 export default {
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import $ from 'jquery';
-import api from './api';
+import $ from "jquery";
+import api from "./api";
+import store from "./store";
 
-import 'normalize.css';
-import './index.css';
+import "normalize.css";
+import "./index.css";
 
-import shoppingList from './shopping-list';
+import shoppingList from "./shopping-list";
 
-const main = function () {
-
-  api.createItem('pears')
+const main = function() {
+  api
+    .getItems()
     .then(res => res.json())
-    .then((newItem) => {
-      return api.getItems();
-    })
-    .then(res => res.json())
-    .then((items) => {
-      console.log(items);
+    .then(items => {
+      items.forEach(item => store.addItem(item));
+      shoppingList.render();
     });
 
   api.getItems();
