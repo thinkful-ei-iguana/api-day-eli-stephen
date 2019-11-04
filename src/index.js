c24fdd6512151794d494e798b29baf22e8048cd7
jpegzilla <eris@jpegzilla.com>
2019-11-04 11:01:45 -0500

    updated api.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import $ from 'jquery';
-import api from './api';
+import $ from "jquery";
+import api from "./api";
 
-import 'normalize.css';
-import './index.css';
+import "normalize.css";
+import "./index.css";
 
-import shoppingList from './shopping-list';
+import shoppingList from "./shopping-list";
 
-const main = function () {
-  api.getItems()
-    .then(res => console.log(res));
-
-  console.log(api.BASE_URL);
-
+const main = function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
