1d50e9b1df873cdff6264717b2f053c11d49a286
stephenschab <sschab689@gmail.com>
2019-11-04 09:28:53 -0700

    added create item function

--- src/api.js
+++ src/api.js
@@ -1,9 +1,19 @@
-const BASE_URL = "https://thinkful-list-api.herokuapp.com/eli-stephen";
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/eli-stephen';
 
-const getItems = function() {
-  return fetch(`${BASE_URL}/items`);
+const getItems = function () {
+  return Promise.resolve('A successful response!');
+};
+
+const createItem = function (name) {
+  const newItem = JSON.stringify({
+    name: name
+  });
+  console.log(newItem);
+  return fetch(`${BASE_URL}/items`, {method: 'POST',   headers: new Headers({'Content-Type':        'application/json'}),
+    body: newItem});
 };
 
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import $ from "jquery";
-import api from "./api";
+import $ from 'jquery';
+import api from './api';
 
-import "normalize.css";
-import "./index.css";
+import 'normalize.css';
+import './index.css';
 
-import shoppingList from "./shopping-list";
+import shoppingList from './shopping-list';
 
-const main = function() {
+const main = function () {
+
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
+
+  api.getItems();
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
