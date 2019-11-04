71cca71dfb81f313dac9ef00f3f2e94057e595ab
jpegzilla <eris@jpegzilla.com>
2019-11-04 12:01:47 -0500

    added updateItem

--- src/api.js
+++ src/api.js
@@ -18,7 +18,17 @@ const createItem = function(name) {
   });
 };
 
+const updateItem = function(id, updateData) {
+  const json = JSON.stringify(updateData);
+  return fetch(`${BASE_URL}/items/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: json
+  });
+};
+
 export default {
   getItems,
-  createItem
+  createItem,
+  updateItem
 };
